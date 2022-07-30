705d20c66c5266e44ae6fcbfa7847402a9c70f67
Abrroo <akhmetov.ersyn@gmail.com>
2022-07-30 13:57:35 +0300

    gg

--- homework3-5-1/homework3-5-1.cpp
+++ homework3-5-1/homework3-5-1.cpp
@@ -1,11 +1,53 @@
-﻿// homework3-5-1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <cstring>
+#include <string>
 
-#include <iostream>
+class Figure
+{
+protected:
+    int count_side_quadrangle = 4;
+    int count_side_triangle = 3;
+    int count_side_none = 0;
+public:
+    int get_c_s_f()
+    {
+        return this->count_side_none;
+    }
+};
+
+class Triangle : protected Figure
+{
+public:
+    Triangle() : Figure() {}
+    int get_c_s_t()
+    {
+        return this->count_side_triangle;
+    }
+};
+
+class Quadrangle : protected Figure
+{
+public:
+    Quadrangle() : Figure() {}
+    int get_c_s_q()
+    {
+        return this->count_side_quadrangle;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+
+    Triangle triangle;
+    Quadrangle quadrangle;
+    Figure figure;
+    std::cout << "Количество сторон :" << std::endl;
+    std::cout << "Фигура: " << figure.get_c_s_f() << std::endl;
+    std::cout << "Треугольник: " << triangle.get_c_s_t() << std::endl;
+    std::cout << "Четырехугольник: " << quadrangle.get_c_s_q() << std::endl;
+
+
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
